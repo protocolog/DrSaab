164ef9b3efed5a1b994c3ad4fc750e45acfd8f40
Deepak Soni <43544330+protocolog@users.noreply.github.com>
2022-09-10 20:02:35 +0530

    Add files via upload

---
+++ MedicalAPI/MedicalApi/wsgi.py
@@ -0,0 +1,21 @@
+"""
+WSGI config for MedicalApi project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
+"""
+
+import os
+import sys
+
+
+from django.core.wsgi import get_wsgi_application
+sys.path.append("/home/MedicalApi/MedicalApi/MedicalApi")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MedicalApi.settings')
+
+application = get_wsgi_application()
+
+os.environ['HTTPS'] = "on"
+
